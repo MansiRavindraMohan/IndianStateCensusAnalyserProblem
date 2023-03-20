69bede75a864b9ffce8597686a0e5ea2251b20d9
MansiRavindraMohan <manasimohan29@gmail.com>
2023-03-20 22:50:10 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IndianStateCensusAnalyserProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian state census analyser problem");
+        }
+    }
+}
